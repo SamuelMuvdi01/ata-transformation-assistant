a1026196de6fcfc92116b5d36fa4549eb4ede6e5
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2025-05-13 14:16:30 +0000

    made changes to import list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ import os
 
 from langchain.llms import Ollama
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 # Streamlit setup
 st.set_page_config(page_title="Ata-transform-bot")
